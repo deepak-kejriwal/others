e6542a4072d7b62cdbf18541048d092ceeeb1082
Deepak <deepak.kejriwal@gmail.com>
2018-12-01 21:53:07 -0600

    There is nothing important to put on commit

--- TestInBuiltJavaFeatures/src/com/coders/ArraysBinarySearch.java
+++ TestInBuiltJavaFeatures/src/com/coders/ArraysBinarySearch.java
@@ -12,6 +12,7 @@ public class ArraysBinarySearch {
 	public static void main(String[] args) {
 		int[] nums= {1,2,3,6,5,4};
 		Arrays.sort(nums);	
+		//Print Array
 		System.out.println("Given input: "+Arrays.toString(nums));
 		int x=Arrays.binarySearch(nums, 3);
 		System.out.println(x+"  -> Output is 2 which is index of number 3");
---
+++ TestInBuiltJavaFeatures/src/com/coders/CreateCopyOfHashSet.java
@@ -0,0 +1,67 @@
+package com.coders;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * 
+ * @author deepak1037
+ *
+ */
+
+/*
+ * Generally we use new HashSet<>() and then addAll method to create copy of
+ * HashSet. Below example demonstrate that it will work only if value is
+ * Immutable. For Immutable one need to create separate copy of value.
+ */
+public class CreateCopyOfHashSet {
+
+	public static void main(String[] args) {
+		Set<List<Integer>> set = new HashSet<>();
+		List<Integer> list1 = Stream.of(1, 2, 3).collect(Collectors.toList());
+		List<Integer> list2 = Stream.of(1, 2, 3, 4).collect(Collectors.toList());
+		set.add(list1);
+		set.add(list2);
+		System.out.println("Initial Set Value");
+		System.out.println("Set: " + set);
+		// Create copy1 using addAll
+		Set<List<Integer>> copy1 = createCopyMethod1(set);
+		// Using copy2 by creating separate copy of value
+		Set<List<Integer>> copy2 = createCopyMethod2(set);
+		// Modifying copy1 by adding 10 to each list
+		for (List<Integer> list : copy1) {
+			list.add(10);
+		}
+//Printing Set, Copy1 and Copy2. Observation: Modification of Copy1 lead to modification lead to modification of Original Set.
+		System.out.println("Copy1:        " + copy1);
+		System.out.println("Original Set: " + set);
+		System.out.println("Copy2:        " + copy2);
+		// Now modifying copy2 by adding 20 to each list
+		for (List<Integer> list : copy2) {
+			list.add(20);
+		}
+//Observation, modifying copy2 doesn't impact other sets (Original Set and copy1).		
+		System.out.println("Copy1:        " + copy1);
+		System.out.println("Original Set: " + set);
+		System.out.println("Copy2:        " + copy2);
+		System.out.println("Hence copy2 is a true copy of set.");
+	}
+
+	private static Set<List<Integer>> createCopyMethod2(Set<List<Integer>> set) {
+		Set<List<Integer>> set2 = new HashSet<>();
+		for (List<Integer> list : set) {
+			set2.add(new ArrayList<Integer>(list));
+		}
+		return set2;
+	}
+
+	private static Set<List<Integer>> createCopyMethod1(Set<List<Integer>> set) {
+		Set<List<Integer>> set2 = new HashSet<>();
+		set2.addAll(set);
+		return set2;
+	}
+}
