c2d30cc51c18bfa4e01fed56ee23b402b11bdb55
Deepak <deepak.kejriwal@gmail.com>
2018-12-20 20:52:01 -0600

    New feature

---
+++ TestInBuiltJavaFeatures/src/com/coders/StringSplit.java
@@ -0,0 +1,16 @@
+package com.coders;
+
+/**
+* 
+* @author Deepak Kejriwal
+*
+*/
+public class StringSplit {
+
+	public static void main(String[] args) {
+		String str="a b c d";
+		System.out.println(str.split(" ", 2)[1]);
+
+	}
+
+}
