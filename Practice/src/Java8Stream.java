bc8e8790bc53173853a89bce09cdab6de21498bd
Deepak <deepak.kejriwal@gmail.com>
2019-08-11 22:36:57 -0500

    Javadocs added

--- Practice/src/Java8Stream.java
+++ Practice/src/Java8Stream.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -10,20 +11,61 @@ import java.util.stream.Stream;
  */
 public class Java8Stream {
 
+	/**
+	 * To convert Integer array to specific ArrayList and not List. toList() returns
+	 * LinkedList
+	 * 
+	 * @param Integer[] nums
+	 * @return ArrayList<Integer>
+	 */
+	public static ArrayList<Integer> ArrayToArrayList(Integer[] nums) {
+		return Arrays.stream(nums).collect(Collectors.toCollection(ArrayList::new));
+	}
+
+	/**
+	 * Conversion of List to Boxed Array
+	 * 
+	 * @param List<Integer>
+	 * 
+	 * @return Integer[]
+	 * 
+	 * 
+	 */
 	public static Integer[] ListToArray(List<Integer> list) {
 		return list.stream().toArray(Integer[]::new);
 	}
 
+	/**
+	 * Conversion of List to Primitive Array
+	 * 
+	 * @param List<Integer>
+	 * 
+	 * @return int[]
+	 * 
+	 * 
+	 */
+	public static int[] ListToArrayPrimitive(List<Integer> list) {
+		return list.stream().mapToInt(x -> x).toArray();
+	}
+
+	/**
+	 * Conversion of primitive array to boxed array
+	 * 
+	 * @param int[]
+	 * @return Integer[]
+	 */
 	public static Integer[] intToInteger(int[] nums) {
 		return Arrays.stream(nums).boxed().toArray(Integer[]::new);
 	}
 
+	/**
+	 * Conversion of boxed array to primitive array
+	 * 
+	 * @param Integer[]
+	 * @return int[]
+	 */
 	public static int[] integerToInt(Integer[] nums) {
 		return Arrays.stream(nums).mapToInt(x -> x).toArray();
-	}
-
-	public static int[] ListToArrayPrimitive(List<Integer> list) {
-		return list.stream().mapToInt(x -> x).toArray();
 	}
 
 	public static void main(String[] args) {
