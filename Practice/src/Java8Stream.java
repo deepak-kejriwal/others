9e7148f850d9f8538d8fb766ef1a326423512038
Deepak <deepak.kejriwal@gmail.com>
2019-08-12 20:57:50 -0500

    Added javadocs from Collector class and few methods

--- Practice/src/Java8Stream.java
+++ Practice/src/Java8Stream.java
@@ -1,15 +1,82 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import static java.util.stream.Collectors.*;
+import static java.util.Map.Entry.*;
 
 /**
  * 
  * @author Deepak Kejriwal
  *
  */
+/**
+ * Implementations of {@link Collector} that implement various useful reduction
+ * operations, such as accumulating elements into collections, summarizing
+ * elements according to various criteria, etc.
+ *
+ * <p>
+ * The following are examples of using the predefined collectors to perform
+ * common mutable reduction tasks:
+ *
+ * <pre>
+ * {@code
+ *     // Accumulate names into a List
+ *     List<String> list = people.stream().map(Person::getName).collect(Collectors.toList());
+ *
+ *     // Accumulate names into a TreeSet
+ *     Set<String> set = people.stream().map(Person::getName).collect(Collectors.toCollection(TreeSet::new));
+ *
+ *     // Convert elements to strings and concatenate them, separated by commas
+ *     String joined = things.stream()
+ *                           .map(Object::toString)
+ *                           .collect(Collectors.joining(", "));
+ *
+ *     // Compute sum of salaries of employee
+ *     int total = employees.stream()
+ *                          .collect(Collectors.summingInt(Employee::getSalary)));
+ *
+ *     // Group employees by department
+ *     Map<Department, List<Employee>> byDept
+ *         = employees.stream()
+ *                    .collect(Collectors.groupingBy(Employee::getDepartment));
+ *
+ *     // Compute sum of salaries by department
+ *     Map<Department, Integer> totalByDept
+ *         = employees.stream()
+ *                    .collect(Collectors.groupingBy(Employee::getDepartment,
+ *                                                   Collectors.summingInt(Employee::getSalary)));
+ *
+ *     // Partition students into passing and failing
+ *     Map<Boolean, List<Student>> passingFailing =
+ *         students.stream()
+ *                 .collect(Collectors.partitioningBy(s -> s.getGrade() >= PASS_THRESHOLD));
+ *
+ * }
+ * </pre>
+ *
+ */
 public class Java8Stream {
+
+	public static void joinListOfStrings(List<String> list) {
+		// Method-1, using stream
+		String str1 = list.stream().collect(Collectors.joining());
+		// Method-2, using String.join
+		String str2 = String.join("", list);
+	}
+
+	public static Map<String, Integer> hashMapSortByValue(Map<String, Integer> input) {
+		Stream<Entry<String, Integer>> entries = input.entrySet().stream();
+		Map<String, Integer> sorted = entries.sorted(comparingByValue())
+				.collect(toMap(e -> e.getKey(), e -> e.getValue(), (e1, e2) -> e2, LinkedHashMap::new));
+		// toMap(key,value,mergeFunction if duplicate key exist,type of map)
+		return sorted;
+	}
 
 	/**
 	 * To convert Integer array to specific ArrayList and not List. toList() returns
@@ -68,9 +135,16 @@ public class Java8Stream {
 		return Arrays.stream(nums).mapToInt(x -> x).toArray();
 	}
 
+	/**
+	 * 
+	 * 
+	 */
 	public static void main(String[] args) {
-		testListToArray();
-		testListToArrayPrimitive();
+		List<String> list = Stream.of("a", "b", "c").collect(Collectors.toList());
+		String str = list.stream().collect(Collectors.joining());
+		System.out.println(str);
+		String str2 = String.join("", list);
+		System.out.println(str2);
 	}
 
 	public static void testListToArray() {
