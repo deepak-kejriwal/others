ade226b5a9cf662fd528d93b0461843af7dca912
deepak <deepak@LAPTOP-J8NMOBS4>
2020-05-30 00:42:53 -0700

    Stack versus Deque behaviour

---
+++ TestInBuiltJavaFeatures/src/com/coders/StackVsDeque.java
@@ -0,0 +1,27 @@
+package com.coders;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class StackVsDeque {
+
+	public static void main(String[] args) {
+		Stack<String> stack = new Stack<>();
+		stack.push("a");
+		stack.push("b");
+		stack.push("c");
+		stack.push("d");
+		System.out.println(String.join(":", stack));
+		//Output: a:b:c:d , which follow FIFO opposite of LIFO of Stack
+		
+		Deque<String> deque = new LinkedList<>();
+		deque.push("a");
+		deque.push("b");
+		deque.push("c");
+		deque.push("d");
+		System.out.println(String.join(":", deque));
+		//Output: d:c:b:a , which follow LIFO pattern of Stack
+	}
+
+}
