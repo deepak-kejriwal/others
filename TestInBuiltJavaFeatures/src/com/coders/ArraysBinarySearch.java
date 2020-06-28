691c6ba2fa20544a7544eeccc8198efe292b2029
deepak <deepak@LAPTOP-J8NMOBS4.T-mobile.com>
2020-06-28 11:44:11 -0700

    Updated

--- TestInBuiltJavaFeatures/src/com/coders/ArraysBinarySearch.java
+++ TestInBuiltJavaFeatures/src/com/coders/ArraysBinarySearch.java
@@ -10,14 +10,16 @@ import java.util.Arrays;
 public class ArraysBinarySearch {
 
 	public static void main(String[] args) {
-		int[] nums= {1,2,3,6,5,4};
+		int[] nums= {1,2,3,4,9};
 		Arrays.sort(nums);	
 		//Print Array
 		System.out.println("Given input: "+Arrays.toString(nums));
 		int x=Arrays.binarySearch(nums, 3);
 		System.out.println(x+"  -> Output is 2 which is index of number 3");
 		int y=Arrays.binarySearch(nums, 7);
-		System.out.println(y+" -> Output is -7 which is -ve of (array length+1) as number 7 not found in input array");
+		System.out.println(y+" -> Output is -5 which is -ve of (insertion index+1) as number 5 not found in input array");
+		int z=Arrays.binarySearch(nums, 10);
+		System.out.println(z+" -> Output is -6 which is -ve of (insertion index+1) as number 10 not found in input array");
 
 	}
 
